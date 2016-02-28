1370851cf350eca424454893be15633614da44bb
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2016-02-28 12:10:34 -0300

    removing unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: {format: :json} do
-    resources :events
-    resources :dances
-    resources :profiles
-  end
   get '/docs', to: redirect('/index.html')
 end
